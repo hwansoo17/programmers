b18714e17de7ebfcd52b2903ad467363206d5b7f
suhwan kim <138691211+hwansoo17@users.noreply.github.com>
2025-08-10 05:23:21 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.95 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,11 +1,15 @@
 function solution(s) {
     var answer = [];
+    const hash = {}
     const a = s.split('')
-    console.log(a)
-    a.forEach((b, i) => {
-        const before = a.slice(0,i).reverse()
-        const position = before.findIndex((c) => c === b)
-        answer.push(position === -1 ? -1 : position + 1)
+    a.forEach((alphabet, i) => {
+        if (hash[alphabet] !== undefined) {
+            answer.push(i - hash[alphabet])
+        } else {
+            answer.push(-1)
+        }
+        hash[alphabet] = i
+   
     })
     return answer;
 }
